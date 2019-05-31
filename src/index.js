cd2ac7f11b82bbbb2d2af38c206a8fae249f8f7a
Badur <basaran.badur@nokia.com>
2019-05-31 15:00:49 +0300

    Added API call and return value when added

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const api = express();
+api.use(express.static(__dirname + "/public")); // middleware
+
+api.use(bodyParser.json()); //middleware
+
+api.listen(3000, () => {
+  console.log("API up and running!");
+});
+
+/* api.get("/", (req, res) => {
+  res.send("Hello world");
+});
+ */
+
+api.post("/add", (req, res) => {
+  console.log(req.body);
+  res.send("It works");
+});
